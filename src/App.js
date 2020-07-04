f828e5b3d60b838dea7922d3d1332ec9d995c44c
Lena Odnoshivkina <elena.odnoshivkina@mail.ru>
2020-07-04 21:11:15 +0300

    creating layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Layout from "./hoc/Layout/Layout";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <Layout>
+        <div style={{width: 400, border: '1px solid black'}}>
+          <h1>Layout works</h1>
+        </div>
+      </Layout>
   );
 }
 
